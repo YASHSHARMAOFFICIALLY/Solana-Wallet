0bbf421716a3d7d8720c2e7e5d6439e0c484537f
Yash sharma <yashsharmaofficially@gmail.com>
2026-01-12 03:04:45 +0530

    fixed ui

--- wallet/components/footer.tsx
+++ wallet/components/footer.tsx
@@ -10,6 +10,15 @@ const Footer = () => {
           <Link href={"https://github.com/YASHSHARMAOFFICIALLY"} className="font-bold">
             @Yash
           </Link>
+           <span className="hidden md:inline">•</span>
+                <Link
+                    href="https://github.com/YASHSHARMAOFFICIALLY/Solana-Wallet"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline hover:text-blue-600"
+                >
+                    ⭐ Star me on GitHub
+                </Link>
         </p>
       </div>
     </section>
